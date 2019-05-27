9f3ce7ce2c4bb33eff031f9a769621bc9db4241e
wenq97 <wenq97@uw.edu>
2019-05-27 16:29:57 -0700

    Added starter files for time series analysis

---
+++ ks-launch-time.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import main
+
+
+def run():
+    data = main.preprocess_data('ks-projects-201801.csv')
+
+
+if __name__ == '__main__':
+    run()
