4d1ddfe8ad61e80b62f1a2e3481735b06e28b89f
Paul Pham <intersect@D-10-18-246-73.dhcp4.washington.edu>
2019-06-04 11:26:40 -0700

    visualize summary statistics

--- problems-2-and-4.py
+++ problems-2-and-4.py
@@ -6,6 +6,7 @@
 
 import math
 from main import preprocess_data as preprocess
+import matplotlib.pyplot as plt
 
 
 def cut_and_arrange(data, percent):
@@ -58,6 +59,7 @@ def success_per_country(data, state):
 def main():
     data = preprocess('ks-projects-201801.csv')
 
+    '''
     # .10 in this case indicates the top 10% of the data
     subset = cut_and_arrange(data, .10)
     unique_categories = get_unique(subset, 'main_category')
@@ -66,11 +68,16 @@ def main():
     # for output confirmation purposes only
     print(unique_categories)
     print(percentages)
-
-    success_rates = success_per_country(data, 'successful')
+    '''
+    success_rates = success_per_country(data, 'successful').to_frame()
 
     # TODO: visualize percentages (pie chart? bar chart?)
-    success_rates.plot('bar')
+    success_rates.plot(kind='bar', legend=False)
+    plt.title('Kickstarter Project Success Rates Per Country')
+    plt.xlabel('Countries')
+    plt.ylabel('Percent Projects Successful')
+    plt.savefig('success_rates.png')
+    plt.show()
 
 
 if __name__ == '__main__':
