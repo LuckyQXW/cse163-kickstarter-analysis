5f67a5442c587e1bd2aa6861221a84053547e811
Paul Pham <intersect@D-10-18-246-73.dhcp4.washington.edu>
2019-06-07 13:53:08 -0700

    add plt charts/visualizations

--- statistical-analysis.py
+++ statistical-analysis.py
@@ -57,40 +57,51 @@ def success_per_country(data, state):
     return of_state
 
 
-def graph(data, percentile):
-    title = percentile + ' Percentile of the Data'
+def graph_perc(data, percentile, pathname):
+    colors = ['#C0392B', '#E74C3C', '#9B59B6', '#8E44AD', '#2980B9',
+              '#3498DB', '#1ABC9C', '#16A085', '#27AE60', '#2ECC71',
+              '#F1C40F', '#F39C12', '#E67E22', '#D35400', '#CD6155']
+    title = 'Categories of the ' + percentile + ' Percentile of the Data'
     labels = list(data.keys())
-
-    plt.pie(data.values(), labels=labels)
+    explode = (0, 0, 0, 0, 0, 0, 0.1, 0, 0, 0, 0, 0, 0, 0, 0)
+    fig, ax = plt.subplots(figsize=(10, 10))
+    plt.rcParams['font.size'] = 15
+    ax.pie(data.values(), labels=labels, colors=colors, startangle=10,
+           explode=explode)
     plt.title(title)
-
-    plt.show()
-    # return plot
+    path = 'results/' + pathname + '_perc_categories.jpg'
+    plt.savefig(path)
 
 
 def main():
     data = preprocess('ks-projects-201801.csv')
 
-    # .10 in this case indicates the top 10% of the data
-    # fifth = cut_and_arrange(data, 0.05)
+    # cut and store the data based on percentiles
+    first = cut_and_arrange(data, .01)
+    fifth = cut_and_arrange(data, 0.05)
     tenth = cut_and_arrange(data, 0.10)
-    # twenty_fifth = cut_and_arrange(data, 0.25)
-    unique_categories = get_unique(tenth, 'main_category')
-    percentages = sample_statistics(tenth, unique_categories, 'main_category')
 
-    graph(percentages, "Tenth")
-    '''
+    # get unique categories
+    categories = get_unique(data, 'main_category')
+
+    first = sample_statistics(first, categories, 'main_category')
+    fifth = sample_statistics(fifth, categories, 'main_category')
+    tenth = sample_statistics(tenth, categories, 'main_category')
+
+    graph_perc(first, '1st', 'first')
+    graph_perc(fifth, '5th', 'fifth')
+    graph_perc(tenth, '10th', 'tenth')
+
     success_rates = success_per_country(data, 'successful').to_frame()
     success_rates = success_rates.sort_values('state', ascending=False)
 
-    # TODO: visualize percentages (pie chart? bar chart?)
     success_rates.plot(kind='bar', legend=False, figsize=(12, 9))
     plt.rcParams['font.size'] = 25
     plt.title('Kickstarter Project Success Rates Per Country')
     plt.xlabel('Countries')
     plt.ylabel('Percent Projects Successful')
-    plt.savefig('success_rates.jpg')
-    '''
+    plt.savefig('results/success_rates_per_country.jpg')
+
     # write results to an output file
     # create and save visualizations for summary statistics (nested pie)
 
