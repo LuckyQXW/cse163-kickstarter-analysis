05bdb6c0cb2b4a98506b5c797ba6931730d77b1b
Paul Pham <intersect@D-10-18-246-73.dhcp4.washington.edu>
2019-06-07 10:55:54 -0700

    add misc changes

--- problems-2-and-4.py
+++ problems-2-and-4.py
@@ -48,6 +48,7 @@ def sample_statistics(data, categories, feature):
 def success_per_country(data, state):
     '''
     Returns a dataframe with the rates of the given project state per country
+    Problem 4
     '''
     successes = data['state'] == state
     total_projects = data.groupby('country')['state'].count()
@@ -56,32 +57,42 @@ def success_per_country(data, state):
     return of_state
 
 
+def graph(data, percentile):
+    title = percentile + ' Percentile of the Data'
+    labels = list(data.keys())
+
+    plt.pie(data.values(), labels=labels)
+    plt.title(title)
+
+    plt.show()
+    # return plot
+
+
 def main():
     data = preprocess('ks-projects-201801.csv')
 
     # .10 in this case indicates the top 10% of the data
-    fifth = cut_and_arrange(data, 0.05)
+    # fifth = cut_and_arrange(data, 0.05)
     tenth = cut_and_arrange(data, 0.10)
-    twenty_fifth = cut_and_arrange(data, 0.25)
+    # twenty_fifth = cut_and_arrange(data, 0.25)
     unique_categories = get_unique(tenth, 'main_category')
     percentages = sample_statistics(tenth, unique_categories, 'main_category')
 
-    # for output confirmation purposes only
-    print(unique_categories)
-    print(percentages)
-
+    graph(percentages, "Tenth")
+    '''
     success_rates = success_per_country(data, 'successful').to_frame()
+    success_rates = success_rates.sort_values('state', ascending=False)
 
     # TODO: visualize percentages (pie chart? bar chart?)
-    success_rates.plot(kind='bar', legend=False)
+    success_rates.plot(kind='bar', legend=False, figsize=(12, 9))
+    plt.rcParams['font.size'] = 25
     plt.title('Kickstarter Project Success Rates Per Country')
     plt.xlabel('Countries')
     plt.ylabel('Percent Projects Successful')
-    # plt.savefig('success_rates.jpg')
-    plt.show()
-
+    plt.savefig('success_rates.jpg')
+    '''
     # write results to an output file
-    # create and save visualizations for summary statistics
+    # create and save visualizations for summary statistics (nested pie)
 
 
 if __name__ == '__main__':
